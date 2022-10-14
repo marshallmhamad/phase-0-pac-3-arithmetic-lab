dcc8ba033a15ace479d09410dfdab039ce640ffc
marshall <mhamad.marshall@gmail.com>
2022-10-14 20:55:58 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+      }
+    function subtract(a, b) {
+    return a - b;
+    }
+    function multiply(a, b) {
+    return a * b;
+    }
+    function divide (a, b) {
+    return a / b;
+    }
+    function increment(n) {
+      return n += 1;
+    }
+    function decrement(n) {
+      return n -= 1;
+    }
+    function makeInt(n) {
+     return Number.parseInt(NaN)
+    }
+    function  makeInt(n) {
+      return Number.parseInt(n)
+    }
+    function preserveDecimal(n) {
+    return  Number.parseFloat(NaN)
+    }
+    function preserveDecimal(n) {
+    return  Number.parseFloat(n)
+    }
+    function makeInt(n) {
+     return parseInt(n, 10)
+    }
